e50b1dcec118e7b5be0191328fdfeda0b9344fb6
Stephen Keller <srkeller@uvm.edu>
2020-01-21 12:48:59 -0500

    purging old Centaurea data

--- Spring_2020/myscripts/CallSNPs.sh
+++ Spring_2020/myscripts/CallSNPs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+########################################################################
+# Script for calling variants using bcftools mpileup and call commands
+# Feb 2018
+# S.R. Keller
+########################################################################
+
+
 #!/bin/bash
 
 # convert to bam
@@ -8,8 +15,7 @@ samtools view -b -@ 4 WA_PP1_F1.sam -o WA_PP1_F1.bam
 
 # check stats on bam alignment file
 
-samtools flagstat WA_PP1_F1.bam >WA_PP1_F1.bamstats
-samtools depth WA_PP1_F1.bam >WA_PP1_F1.depth
+samtools flagstat WA_PP1_F1.bam
 
 # fix mate pairs
 
@@ -21,12 +27,18 @@ samtools  sort -@ 4 WA_PP1_F1_fixmate.bam -o WA_PP1_F1_fixmate.sorted.bam
 
 # mark and remove duplicates
 
+# /data/popgen/sambamba_v0.6.0 markdup -t 4 -r -p WA_PP1_F1_fixmate.sorted.bam WA_PP1_F1_fixmate.sorted.rmdup.bam
+
 samtools rmdup WA_PP1_F1_fixmate.sorted.bam WA_PP1_F1_fixmate.sorted.rmdup.bam
 
 # index
 
 samtools index WA_PP1_F1_fixmate.sorted.rmdup.bam
 
-# call SNPs with samtools/bcftools
+# call SNPs with bcftools
 
-
+bcftools mpileup \
+  -Ou -f /data/project_data/beetles/reference/OTAU.fna /data/project_data/beetles/sam/WA_PP1_F1_fixmate.sorted.rmdup.bam \
+  -C 50 --min-MQ 20 --min-BQ 20 --threads 4 --skip-indels --annotate AD,DP | \
+  bcftools call -Ov -mv --format-fields GQ >WA_PP1_F1.vcf
+ 
